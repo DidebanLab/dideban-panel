044a8e4a3e9b867f99029a07a1619d4c701e8266
seyed mahdi hosseini <onlinetraders.mahdi.hosseini@gmail.com>
2026-02-09 11:15:19 +0330

    💄fix gap of history detail of agents section in dashboard

--- src/components/pages/dashboard/statusOverview/AgentsStatusOverview.svelte
+++ src/components/pages/dashboard/statusOverview/AgentsStatusOverview.svelte
@@ -175,7 +175,9 @@
                     : 'bg-[#FFFFFF]/5'}">
                 {#if historyDetail}
                   <div
-                    class="absolute w-fit group-hover:flex hidden bottom-10 start-1/2 -translate-x-1/2 rounded-xl text-white bg-white/40 dark:bg-black/80 backdrop-blur-md dark:backdrop-blur-3xl border-[#0D0D0D]/5 border dark:border-white/10 px-3 py-2 flex-col justify-start items-start gap-2.5">
+                    class="absolute w-fit group-hover:flex hidden bottom-10 start-1/2 -translate-x-1/2 rounded-xl text-white bg-white/40 dark:bg-black/80 backdrop-blur-md dark:backdrop-blur-3xl border-[#0D0D0D]/5 border dark:border-white/10 px-3 py-2 flex-col justify-start items-start {historyDetail.is_offline
+                      ? 'gap-1.5'
+                      : 'gap-2.5'}">
                     {#if historyDetail.collect_duration_ms}
                       <div
                         class="w-full flex justify-between items-center gap-2.5 border-b border-b-[#0D0D0D]/10 dark:border-b-white/15 pb-1.5">
