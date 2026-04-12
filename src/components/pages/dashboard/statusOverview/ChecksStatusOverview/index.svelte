93a82dd13199c7ee4e8ff6a21f39c81161b23254
seyed mahdi hosseini <onlinetraders.mahdi.hosseini@gmail.com>
2026-04-12 04:29:11 +0330

    💚fix bug of build

--- src/components/pages/dashboard/statusOverview/ChecksStatusOverview/index.svelte
+++ src/components/pages/dashboard/statusOverview/ChecksStatusOverview/index.svelte
@@ -8,7 +8,6 @@
   import responseTimeColor from '../../../../../utils/responseTimeColor.js';
   import { on, off } from '../../../../../services/ws.svelte.js';
   import History from './deps/History.svelte';
-  import HistoryLoading from '../HistoryLoading.svelte';
 
   let trigger = $state(0);
   let checks = $state(null);
@@ -97,7 +96,30 @@
       : ' grid-cols-1 xl:grid-cols-2 3xl:grid-cols-3'}">
     {#if loading}
       {#each Array(3) as _, i}
-        <HistoryLoading requiredHistoryCount={REQUIRED_HISTORY_COUNT} />
+        <div
+          class="relative h-29 sm:h-32.75 border border-[#0D0D0D]/5 dark:border-gray-400/5 rounded-[14px] flex flex-col py-4 xl:py-6 gap-7">
+          <div class="flex justify-start items-center px-4.25 gap-4">
+            <div class="size-12 rounded-2xl hidden sm:block bg-white/5 animate-pulse"></div>
+
+            <div class="flex flex-col justify-center items-start gap-1">
+              <span class="w-20 h-5 bg-white/5 rounded-md animate-pulse"></span>
+
+              <div class="w-40 h-4 bg-white/5 mt-1 rounded-md animate-pulse"></div>
+            </div>
+            <div class="flex justify-center items-center gap-2 ms-auto mb-auto">
+              <span class="w-10 h-4 bg-white/5 animate-pulse rounded-md"> </span>
+
+              <div class="size-3 rounded-full bg-white/5 animate-pulse"></div>
+            </div>
+          </div>
+
+          <div
+            class="absolute z-10 bottom-4 xl:bottom-6 w-full flex gap-0.5 justify-between items-end px-4.25 animate-pulse">
+            {#each Array(REQUIRED_HISTORY_COUNT) as _, i}
+              <div class="w-full h-4 rounded-[1px] bg-white/5"></div>
+            {/each}
+          </div>
+        </div>
       {/each}
     {:else if checks}
       {#each checks as item, i}
